1adbe15d9a2097d65ab09329ac0bd4a8a97d13db
KauanS2 <kauans2dev@gmail.com>
2023-08-10 20:18:51 -0300

    feat-checkout-adress

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -4,32 +4,71 @@ import {
   CheckoutContainer,
   CompleteOrder,
   ConfirmOrder,
+  FormAdress,
+  IconAdress,
+  IconPayment,
+  InputAdress,
   Payment,
+  PaymentChoice,
 } from './styles'
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
 
 export function Checkout() {
   return (
     <CheckoutContainer>
       <CompleteOrder>
         <Adress>
-          <span>Endereço</span>
-          <div>
-            <form action="">
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-            </form>
-          </div>
+          <IconAdress>
+            <MapPinLine size={22} />
+            <div>
+              <p>Endereço de Entrega</p>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+          </IconAdress>
+          <FormAdress>
+            <InputAdress placeholder="CEP" id="intOne" type="text" />
+            <InputAdress placeholder="Rua" id="intTwo" type="text" />
+            <div>
+              <InputAdress placeholder="Número" id="intThree" type="text" />
+              <InputAdress placeholder="Complemento" id="intFour" type="text" />
+            </div>
+            <div>
+              <InputAdress placeholder="Bairro" id="intFive" type="text" />
+              <InputAdress placeholder="Cidade" id="intSix" type="text" />
+              <InputAdress placeholder="UF" id="intSeven" type="text" />
+            </div>
+          </FormAdress>
         </Adress>
         <Payment>
-          <span>Pagamento</span>
-          <button>TEste</button>
-          <button>teste</button>
-          <button>teste</button>
+          <IconPayment>
+            <CurrencyDollar size={22} />
+            <div>
+              <p>Pagamento</p>
+              <span>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </span>
+            </div>
+          </IconPayment>
+          <PaymentChoice>
+            <button>
+              <CreditCard size={16} />
+              CARTÃO DE CRÉDITO
+            </button>
+            <button>
+              <Bank size={16} />
+              CARTÃO DE DÉBITO
+            </button>
+            <button>
+              <Money size={16} />
+              DINHEIRO
+            </button>
+          </PaymentChoice>
         </Payment>
       </CompleteOrder>
       <ConfirmOrder>
